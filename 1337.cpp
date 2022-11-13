864d26c6bcee2833bba940a8ff5b69e9f28f7ebe
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-13 16:23:06 +0300

    Add files via upload

---
+++ 1337.cpp
@@ -0,0 +1,25 @@
+﻿// 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b,c;
+    cin >> a >> b;
+    if (b <= a / 2 && b>a/10) {
+        cout << 2;
+    }
+    if (b <= a / 10) {
+        cout << 1;
+    }
+    if (b > a / 2 && b <= a / 4 * 3-4) {
+        cout << 3;
+    }
+    if (b > a / 4 * 3 - 4 && b < a / 10 * 8 + 5) {
+        cout << 4;
+    }
+    if (b >= a / 10 * 8 + 5) {
+        cout << 5;
+    }
+}
+
